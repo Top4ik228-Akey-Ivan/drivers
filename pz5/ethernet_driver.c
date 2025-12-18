dd0a9a6629f3e5ec5687ea52a66225e9dda3a183
Ivan Orekhov <ivanorekhov04022005@gmail.com>
2025-12-19 00:22:20 +0300

    fix md

--- pz5/ethernet_driver.c
+++ pz5/ethernet_driver.c
@@ -4,62 +4,85 @@
 
 #define MODNAME "rawdemo_driver"
 
+// Это главный объект сетевого интерфейса
 static struct net_device *vnet;
 
+// Вызывается, когда делаем ip link set dev ethX up:
 static int drv_open(struct net_device *ndev)
 {
     pr_info(MODNAME ": device opened\n");
+    // Разрешаем ядру передавать пакеты через этот интерфейс
     netif_start_queue(ndev);
     return 0;
 }
-
+// Вызывается при ip link set dev ethX down
 static int drv_stop(struct net_device *ndev)
 {
     pr_info(MODNAME ": device stopped\n");
+    // Запрещаем ядру отправлять пакеты
     netif_stop_queue(ndev);
     return 0;
 }
 
+// skb — это сетевой пакет
+//skb->data — данные пакета
+// skb->len — длина
 static netdev_tx_t drv_send(struct sk_buff *skb, struct net_device *ndev)
 {
+    // Счётчик для циклов
     unsigned int i;
 
     pr_info(MODNAME ": transmit invoked\n");
     pr_info(MODNAME ": skb length=%u\n", skb->len);
 
     pr_info(MODNAME ": RAW HEX: ");
+    // Проходим по каждому байту пакета => Печатаем его в шестнадцатеричном виде
     for (i = 0; i < skb->len; i++)
+    // %02x - формат вывода 2-кол-во знаков x - 16сс
         pr_cont("%02x ", skb->data[i]);
     pr_cont("\n");
 
+    // Печать пакета как ASCII
     pr_info(MODNAME ": ASCII: ");
     for (i = 0; i < skb->len; i++)
         pr_cont("%c ", skb->data[i]);
     pr_cont("\n");
 
+    // это статистика сетевого интерфейса.
+    // Увеличиваем счётчик отправленных пакетов
     ndev->stats.tx_packets++;
+    // Увеличиваем счётчик байтов
     ndev->stats.tx_bytes += skb->len;
 
+    // Мы обязаны освободить skb(пакет), иначе утечка памяти
     dev_kfree_skb(skb);
     return NETDEV_TX_OK;
 }
 
 static const struct net_device_ops drv_ops = {
+    //Когда интерфейс поднимают
     .ndo_open        = drv_open,
+    // Когда интерфейс опускают
     .ndo_stop        = drv_stop,
+    // Когда отправляют пакет
     .ndo_start_xmit  = drv_send,
 };
 
 static int __init drv_init(void)
 {
+    // Создаём Ethernet-устройство / назначается MAC-адрес
     vnet = alloc_etherdev(0);
+    // Если не хватило памяти — ошибка
     if (!vnet)
         return -ENOMEM;
 
+    // Это привязка драйвера к сетевому устройству.
     vnet->netdev_ops = &drv_ops;
 
+    // Регистрируем интерфейс в ядре После этого он появится в ip link
     if (register_netdev(vnet)) {
         pr_err(MODNAME ": failed to register device\n");
+        // Чистим память и выходим
         free_netdev(vnet);
         return -1;
     }
@@ -70,11 +93,15 @@ static int __init drv_init(void)
 
 static void __exit drv_exit(void)
 {
+    // Убираем интерфейс из системы
     unregister_netdev(vnet);
+    // Освобождаем память
     free_netdev(vnet);
     pr_info(MODNAME ": driver unloaded\n");
 }
 
+
+//Указываем ядру: что вызывать при загрузке / что при выгрузке
 module_init(drv_init);
 module_exit(drv_exit);
 
--- pz5/ethernet_sender.c
+++ pz5/ethernet_sender.c
@@ -9,61 +9,90 @@
 #include <linux/if_packet.h>
 #include <linux/if_ether.h>
 
+// Структура Ethernet-кадра
 struct eth_frame {
     struct ethhdr hdr;
     unsigned char payload[ETH_DATA_LEN];
-};
+};  
 
 int main() {
+    // Имя интерфейса, через который отправляем кадр
     const char *iface = "eth0";
+    // MAC назначения
     const unsigned char target_mac[ETH_ALEN] = {0,0,0,0,0,0};
     const char *msg = "hello world";
+    // Дескриптор сокета
     int sockfd;
 
+    // Создание RAW socket
+    // AF_PACKET	Работаем напрямую с Ethernet
+    // SOCK_RAW	Сырые кадры
+    // htons(0x1234)	EtherType
     sockfd = socket(AF_PACKET, SOCK_RAW, htons(0x1234));
+    //если не создался
     if (sockfd < 0) {
         perror("socket");
         return 1;
     }
-
+    // Универсальная структура для ioctl
     struct ifreq ifr;
+    // Команда заполняет область памяти одинаковым байтом То есть: обнуляем всю структуру / Предотвращаем мусор
     memset(&ifr, 0, sizeof(ifr));
+
+    // Говорим ядру: «Я хочу работать с интерфейсом eth0»
     strncpy(ifr.ifr_name, iface, IFNAMSIZ);
 
+    // Получить индекс интерфейса
     if (ioctl(sockfd, SIOCGIFINDEX, &ifr) == -1) {
         perror("SIOCGIFINDEX");
         return 1;
     }
     int index = ifr.ifr_ifindex;
-
+    // Буфер под MAC источника
     unsigned char src_mac[ETH_ALEN];
+    // Получить mac - адрес
     if (ioctl(sockfd, SIOCGIFHWADDR, &ifr) == -1) {
         perror("SIOCGIFHWADDR");
         return 1;
     }
+    // скопировать mac
     memcpy(src_mac, ifr.ifr_hwaddr.sa_data, ETH_ALEN);
 
     struct eth_frame frame;
+    // MAC назначения
     memcpy(frame.hdr.h_dest, target_mac, ETH_ALEN);
+    // MAC источника
     memcpy(frame.hdr.h_source, src_mac, ETH_ALEN);
+    // Тип протокола
     frame.hdr.h_proto = htons(0x1234);
 
+    // длина сообщения
     size_t msglen = strlen(msg);
+    // Копируем "hello world" в payload
     memcpy(frame.payload, msg, msglen);
 
     struct sockaddr_ll addr = {0};
+    // Тип адреса
     addr.sll_family = AF_PACKET;
+    // Через какой интерфейс отправлять
     addr.sll_ifindex = index;
+    // Длина MAC-адреса
     addr.sll_halen = ETH_ALEN;
+    // MAC назначенияx
     memcpy(addr.sll_addr, target_mac, ETH_ALEN);
 
+    // отправка кадра
+    // sockfd	raw socket
+    // &frame	указатель на Ethernet-кадр
+    // msglen + ETH_HLEN	длина кадра
+    // addr	куда отправлять
     if (sendto(sockfd, &frame, msglen + ETH_HLEN, 0,
                (struct sockaddr *)&addr, sizeof(addr)) < 0) {
         perror("sendto");
     } else {
         printf("Frame sent successfully\n");
     }
-
+    // Закрываем сокет
     close(sockfd);
     return 0;
 }
